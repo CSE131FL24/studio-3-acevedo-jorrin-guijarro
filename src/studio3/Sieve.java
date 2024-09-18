aef268792440eae69f01335f6528e69a6947eb51
ericacevedo <ericacevedo@10.232.158.38>
2024-09-18 15:41:57 -0500

    hiiii

---
+++ src/studio3/Sieve.java
@@ -0,0 +1,25 @@
+package studio3;
+
+import java.util.Scanner;
+
+public class Sieve {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		boolean [] sieve = new boolean[n+1];
+	
+		for (int i = 2; i <= n; i++){
+		sieve[i] = true;
+		if (n % 2 == 1) {
+			int byTwo = n/2 ;
+			
+}
+		
+		
+		
+		}
+
+	}
+}
